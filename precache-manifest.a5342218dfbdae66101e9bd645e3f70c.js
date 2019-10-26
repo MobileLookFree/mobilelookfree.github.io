74c4a387ef536f3238cee8dcb66d2fb11d319827
Andrei <mobilelookfree@gmail.com>
2019-10-26 17:14:55 +0300

    fixed gh-pages deploy

---
+++ precache-manifest.a5342218dfbdae66101e9bd645e3f70c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8468644e4ab5c78b6b9019024ee83b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28c1304112c729de3baf",
+    "url": "/static/js/2.0d3b2f03.chunk.js"
+  },
+  {
+    "revision": "dccd34a9cbb2e16e5d66",
+    "url": "/static/js/main.5e80c85f.chunk.js"
+  },
+  {
+    "revision": "12c8b56ceebefce47b59",
+    "url": "/static/js/runtime-main.48814a39.js"
+  }
+]);
