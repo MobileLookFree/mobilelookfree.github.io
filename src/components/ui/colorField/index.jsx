f373c5e28d230feb77a40630ae690da1160138c7
Andrei <mobilelookfree@gmail.com>
2019-11-24 19:10:43 +0300

    fix for mobile devices

--- src/components/ui/colorField/index.jsx
+++ src/components/ui/colorField/index.jsx
@@ -11,6 +11,11 @@ const styles = {
     background: ({ darkTheme }) => (darkTheme ? 'linear-gradient(129.79deg, #59129B 22.05%, #5430E6 50.2%)' : 'linear-gradient(129.79deg, #DBFFFF 22.05%, #FFEAFA 72.03%)'),
     transform: ({ coordinateX }) => `translate3d(-${coordinateX}px, 0px, 0px)`,
   },
+  '@media (pointer: coarse) and (hover: none)': {
+    colorField: {
+      width: '100%',
+    },
+  },
 };
 
 export default withStyles(styles)(ColorField);
